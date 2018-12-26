de75e12e5849d62f3ad799eda237028242459db3
Marchenko Andrey <zeroday343@gmail.com>
2018-12-26 15:42:50 +0300

    added aiml patterns

--- bot/bot.py
+++ bot/bot.py
@@ -1,17 +1,34 @@
+import config
 import constants
 import aiml
 import telebot
+import random
+from googletrans import Translator
 import os
 
 kernel = aiml.Kernel()
 kernel.bootstrap(learnFiles="aiml/startup.xml", commands="LOAD AIML BOT")
 
+translator = Translator()
+
 # telebot.apihelper.proxy = {'https': 'socks5://telegram:telegram@ailtn.tgvpnproxy.me:1080'}
 
-token = os.environ['BOT_TOKEN']
-bot = telebot.TeleBot(token)
+# token = os.environ['BOT_TOKEN']
+bot = telebot.TeleBot(config.token)
 
 is_silenced = False
+
+
+def translate_text(text, dest):
+    if text == "":
+        text = random.choice(constants.random_russian_words)
+        response_text = constants.empty_translate_text_message.format(
+            text,
+            translator.translate(text, dest=dest).text
+        )
+    else:
+        response_text = translator.translate(text, dest=dest).text
+    return response_text
 
 
 @bot.message_handler(commands=["silence"])
@@ -42,6 +59,28 @@ def handle_help(message):
     bot.send_message(message.chat.id, constants.help_ru)
 
 
+@bot.message_handler(commands=["report"])
+def handle_help(message):
+    if is_silenced:
+        return
+    bot.send_message(message.chat.id, constants.random_speech)
+
+
+@bot.message_handler(commands=["translate"])
+def handle_translate(message):
+    command_len = len("/translate") + 1
+
+    text = message.text[command_len:]
+    dest_lang = kernel.getPredicate("translate_dest_lang", message.chat.id)
+    dest_lang = dest_lang if dest_lang in constants.languages else "en"
+
+    if text[:2] in constants.languages:
+        dest_lang = text[:2]
+        text = text[3:]
+    response_text = translate_text(text, dest=dest_lang)
+    bot.send_message(message.chat.id, response_text)
+
+
 @bot.message_handler(func=lambda message: True, content_types=["text"])
 def response(message):
     global is_silenced
@@ -50,11 +89,16 @@ def response(message):
     if message.text == "unsilence_all" or message.text == "UNSILENCE_ALL":
         is_silenced = False
         return
+    if message.text == "report_all" or message.text == "REPORT_ALL":
+        bot.reply_to(message, constants.random_speech)
+        return
     if is_silenced:
         return
     response_text = kernel.respond(message.text, message.chat.id)
-    bot.send_message(message.chat.id, response_text)
+    bot.reply_to(message, response_text)
 
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
+
+
